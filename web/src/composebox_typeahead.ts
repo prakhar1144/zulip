322bd3b056e84a9d0efb52912153675795179384
Karl Stolley <karl@zulip.com>
2025-01-27 15:52:15 -0500

    compose: Immediately open topic typeahead on empty topic box click.

--- web/src/composebox_typeahead.ts
+++ web/src/composebox_typeahead.ts
@@ -1432,6 +1432,7 @@ export function initialize({
         source(): string[] {
             return topics_seen_for(compose_state.stream_id());
         },
+        helpOnEmptyStrings: !realm.realm_mandatory_topics,
         items: max_num_items,
         highlighter_html(item: string): string {
             return typeahead_helper.render_typeahead_item({primary: item});
